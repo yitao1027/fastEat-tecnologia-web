33f06a733db7ee6bb18bb1027caf208f8f1d2c88
Yitao Zhu <yitao.zhu@studio.unibo.it>
2018-06-04 15:19:22 +0200

    nuovo db con vari fix

--- file/fasteat.sql
+++ file/fasteat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Giu 01, 2018 alle 16:23
+-- Creato il: Giu 04, 2018 alle 15:18
 -- Versione del server: 10.1.25-MariaDB
 -- Versione PHP: 7.1.7
 
@@ -32,9 +32,9 @@ USE `fasteat`;
 
 DROP TABLE IF EXISTS `listaprodotto`;
 CREATE TABLE `listaprodotto` (
-  `PiattoN` text NOT NULL,
-  `piattoName` text NOT NULL,
-  `categoria` varchar(64) DEFAULT NULL,
+  `PiattoN` int(11) NOT NULL,
+  `PiattoName` text NOT NULL,
+  `Categoria` varchar(64) DEFAULT NULL,
   `Prezzo` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,17 +42,20 @@ CREATE TABLE `listaprodotto` (
 -- Dump dei dati per la tabella `listaprodotto`
 --
 
-INSERT INTO `listaprodotto` (`PiattoN`, `piattoName`, `categoria`, `Prezzo`) VALUES
-('1', 'nigiri salmone', 'Nigiri', 3),
-('11', 'Hosomaki Furai', 'Hosomaki', 5),
-('12', 'Hosomaki Salmon', 'Hosomaki', 4),
-('2', 'tonno nigiri', 'Nigiri', 4),
-('3', 'ebi nigiri', 'Nigiri', 3),
-('30', 'Acqua naturale 50cl', 'Bevande', 2.5),
-('31', 'CocaCola 33cl', 'Bevande', 3.5),
-('4', 'branzino nigiri', 'Nigiri', 3),
-('5', 'avocado nigiri', 'Nigiri', 4),
-('6', 'sakeAburi nigiri', 'Nigiri', 5);
+INSERT INTO `listaprodotto` (`PiattoN`, `PiattoName`, `Categoria`, `Prezzo`) VALUES
+(1, 'nigiri salmone', 'Nigiri', 3),
+(2, 'tonno nigiri', 'Nigiri', 4),
+(3, 'ebi nigiri', 'Nigiri', 3),
+(4, 'branzino nigiri', 'Nigiri', 3),
+(5, 'avocado nigiri', 'Nigiri', 4),
+(6, 'sakeAburi nigiri', 'Nigiri', 5),
+(7, 'nigiri fantasia', 'Nigiri', 5),
+(11, 'Hosomaki Furai', 'Hosomaki', 5),
+(12, 'Hosomaki Salmon', 'Hosomaki', 4),
+(23, 'hosomaki hosomaki', 'Hosomaki', 6),
+(30, 'Acqua naturale 50cl', 'Bevande', 2.5),
+(31, 'CocaCola 33cl', 'Bevande', 3.5),
+(45, 'Vino Bianco Donna fugata', 'Vino ', 12);
 
 -- --------------------------------------------------------
 
@@ -65,8 +68,16 @@ CREATE TABLE `ordine` (
   `email` varchar(64) NOT NULL,
   `data` date NOT NULL,
   `ora` time NOT NULL,
-  `ordine` varchar(128) NOT NULL
+  `info` text NOT NULL,
+  `ordine` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `ordine`
+--
+
+INSERT INTO `ordine` (`email`, `data`, `ora`, `info`, `ordine`) VALUES
+('123prova@gmail.com', '2018-06-04', '14:22:00', 'Consegna<br>user:123prova@gmail.com<br>cell:12<br>Address:12<br>', '<p>nigiri salmone&emsp; porzione: 3&emsp; totale piatto: â‚¬ 9</p><p>totale:  â‚¬9</p>');
 
 -- --------------------------------------------------------
 
@@ -97,6 +108,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`password`, `email`) VALUES
+('51f9e0e7544f0908a8499fd09354530031f5612b2c36bd1638394082000bd575', '123prova@gmail.com'),
+('1e05a02d9cce7926a5aa1c34183c96f5d333a93f461504055f8d22007d50e864', 'fasteat@gmail.com'),
 ('1e05a02d9cce7926a5aa1c34183c96f5d333a93f461504055f8d22007d50e864', 'zhu.yt1027@gmail.com');
 
 --
@@ -107,13 +120,14 @@ INSERT INTO `users` (`password`, `email`) VALUES
 -- Indici per le tabelle `listaprodotto`
 --
 ALTER TABLE `listaprodotto`
-  ADD PRIMARY KEY (`PiattoN`(8));
+  ADD PRIMARY KEY (`PiattoN`);
 
 --
 -- Indici per le tabelle `ordine`
 --
 ALTER TABLE `ordine`
-  ADD KEY `id` (`email`,`data`,`ora`,`ordine`);
+  ADD PRIMARY KEY (`email`,`data`,`ora`),
+  ADD KEY `id` (`email`,`data`,`ora`) USING BTREE;
 
 --
 -- Indici per le tabelle `tentativilogin`
